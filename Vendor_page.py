4c404d5c11517e2f9adbdd604a6f3903ff0178c1
Priyanka Vijaybaskar <94910461+prinks-wiz@users.noreply.github.com>
2022-08-04 14:52:00 +0530

    Update Vendor_page.py

--- Vendor_page.py
+++ Vendor_page.py
@@ -65,7 +65,7 @@ st.write(order_queue.first())
 order_complete = st.button("Order delivered")
 if order_complete:
     order_queue.dequeue()
-    new_df = df[1:]
+    df.drop([0],axis=0,inplace = "True")
     update_the_spreadsheet('CartList',new_df)
 
 st.write(" ")
